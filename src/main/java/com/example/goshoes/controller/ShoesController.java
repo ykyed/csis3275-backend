09ac2410baff8206df61dc6acc55721dc989109b
Dan <wooastudio1012@gmail.com>
2024-10-07 16:26:23 -0700

    add findAllShoes()

--- src/main/java/com/example/goshoes/controller/ShoesController.java
+++ src/main/java/com/example/goshoes/controller/ShoesController.java
@@ -1,17 +1,19 @@
 package com.example.goshoes.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.goshoes.model.Shoe;
-import com.example.goshoes.model.ShoeRepository;
-import com.example.goshoes.request.ShoeRequest;
+import com.example.goshoes.model.Shoes;
+import com.example.goshoes.model.ShoesRepository;
+import com.example.goshoes.request.ShoesRequest;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +22,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
-public class ShoeController {
+public class ShoesController {
 	
 	@Autowired
-	ShoeRepository shoeRepository;
+	ShoesRepository shoesRepository;
 	
-	@PostMapping("/shoelist")
-	public ResponseEntity<?> shoeList(@RequestBody ShoeRequest shoeRequest) {
+	@PostMapping("/shoes")
+	public ResponseEntity<?> findAllShoes(@RequestBody ShoesRequest shoesRequest) {
 
 		try {
-			Optional<Shoe> shoeData = shoeRepository.findByBrand(shoeRequest.getBrand());
+			List<Shoes> shoesData = shoesRepository.findAll();
+			
+			return new ResponseEntity<>(shoesData, HttpStatus.OK);
+		
+		} catch (Exception e) {
+
+		}
+		return null;
+
+	}
+	
+	@GetMapping("/shoes/brand/{brandName}")
+	public ResponseEntity<?> getShoesByBrand(@RequestBody ShoesRequest shoesRequest) {
+
+		try {
+			Optional<Shoes> shoeData = shoesRepository.getShoesByBrand(shoesRequest.getBrand());
 			if (shoeData.isPresent()) {
 				
 				return new ResponseEntity<>(shoeData.get(), HttpStatus.OK);
