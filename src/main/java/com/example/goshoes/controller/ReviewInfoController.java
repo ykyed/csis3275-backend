8dfbb45714d1372ceed1b33aec45226641cd58b7
Dan <wooastudio1012@gmail.com>
2024-10-26 17:13:00 -0700

    update rating / review value

--- src/main/java/com/example/goshoes/controller/ReviewInfoController.java
+++ src/main/java/com/example/goshoes/controller/ReviewInfoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.goshoes.event.AddReviewEvent;
 import com.example.goshoes.model.ReviewInfo;
 import com.example.goshoes.model.ReviewInfoRepository;
 
@@ -27,7 +29,14 @@ public class ReviewInfoController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ReviewInfoController.class);
 	
-	 @Autowired private ReviewInfoRepository repository;
+	 @Autowired 
+	 private ReviewInfoRepository repository;
+	 
+	 private final ApplicationEventPublisher eventPublisher; 
+	 
+	 public ReviewInfoController(ApplicationEventPublisher eventPublisher) {
+		 this.eventPublisher = eventPublisher;
+	 }
 	  
 	 @GetMapping("/reviews/{productCode}") 
 	 public ResponseEntity<?> getReviewsByProductCode(@PathVariable String productCode) {
@@ -53,6 +62,7 @@ public class ReviewInfoController {
 	 public ResponseEntity<?> addReview(@RequestBody ReviewInfo reviewInfo) {
 	 	
 		 try {
+			 eventPublisher.publishEvent(new AddReviewEvent(reviewInfo.getProductCode(), reviewInfo.getRating()));
 			 repository.save(reviewInfo);
 			 return new ResponseEntity<>("Review added successfully", HttpStatus.CREATED);
 		} 
