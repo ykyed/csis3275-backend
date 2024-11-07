032b4b48053787a739300bfa043f0339c4ec426a
hyunheeee12 <kimh134@student.douglascollege.ca>
2024-11-07 01:47:02 -0800

    add db table name

--- src/main/java/com/example/goshoes/model/ReviewInfo.java
+++ src/main/java/com/example/goshoes/model/ReviewInfo.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "REVIEW_INFO")//추가해
 public class ReviewInfo {
 
 	@Id
